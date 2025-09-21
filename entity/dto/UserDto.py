bd1d5d6636c3fe4a00feea093a0f5be048bc5032
chenzhirong <826531489@qq.com>
2025-09-21 18:55:06 +0800

    test

--- entity/dto/UserDto.py
+++ entity/dto/UserDto.py
@@ -2,8 +2,11 @@ from typing import Optional
 
 from pydantic import Field
 
-from entity.dto.base import BasePageQueryReq
+from entity.dto.base import BasePageQueryReq, BaseQueryReq
 
 
-class UserQueryPageReq(BasePageQueryReq):
-    username: Optional[str]= Field(default=None,description=" asc或 desc")
+class UserQueryReq(BaseQueryReq):
+    username: Optional[str]= Field(default=None,description="名称")
+
+class UserQueryPageReq(UserQueryReq, BasePageQueryReq):
+    pass
--- router/user_app.py
+++ router/user_app.py
@@ -1,10 +1,17 @@
 from fastapi import APIRouter, Query
 
-from entity.dto.UserDto import UserQueryPageReq
+from entity.dto.UserDto import UserQueryPageReq, UserQueryReq
+from router import BaseController
 from service.user_service import UserService
 
 router = APIRouter(prefix="/user", tags=["用户"])
 base_service = UserService
+base_app = BaseController(base_service)
+
 @router.get("/page")
-async def page(req:UserQueryPageReq=Query(...)):
+async def get_page(req:UserQueryPageReq=Query(...)):
     return await base_service.get_by_page(req)
+
+@router.get("/list")
+async def get_list(req:UserQueryReq=Query(...)):
+    return await base_service.get_list(req)
